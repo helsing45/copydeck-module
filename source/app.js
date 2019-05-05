eaf8bade078b9c0960c6096c46f24563129d745d
Jean-Christophe Décary <j-c9@hotmail.com>
2019-05-05 13:01:07 -0400

    Translator
    * Creating Translator class
    * Creating CsvMapTransformation

--- source/app.js
+++ source/app.js
@@ -2,21 +2,36 @@ import AndroidIO from "./IO/AndroidIO";
 import CsvIO from "./IO/CsvIO";
 import IosIO from "./IO/IosIO";
 import I18nextIO from "./IO/I18nextIO";
+import CsvMapTransformation from "./map/CsvMapTransformation";
+import Translator from "./Translator";
 
-var io = new I18nextIO();
+var input = new CsvIO();
+input.read("./files/csv/copydeck-v2.csv");
+var output = new CsvIO();
+
+var translator = new Translator(input, new CsvMapTransformation(),new CsvMapTransformation(),output);
+translator.translate('./files/output/translator');
+
+/*var io = new I18nextIO();
 io.read("./files/i18next");
-io.write("./files/output/i18next");
+io.write("./files/output/i18next");*/
 
 /*var io = new IosIO();
 io.read("./files/iOS");
 io.write("./files/output");*/
 
 /*var io = new CsvIO();
-io.read("./files/csv/read_csv_file_test.csv");
-io.write("./files/output");*/
+io.read("./files/csv/copydeck-v2.csv");
+var csvMap = new CsvMapTransformation();
+var result = csvMap.toBaseForm(io).then((x)=>{
+    let outputFile = new CsvIO();
+    let file = {};
+    file["Test"] = csvMap.fromBaseForm(x);
+    outputFile.file = file;
+    outputFile.write("./files/translate")
+});*/
 
 /*var io =new AndroidIO();
 io.read("./files/android");
-io.write('./files/output');*/
-
-var b = true;
+var b = true;*/
+//io.write('./files/output');
